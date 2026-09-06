90fc881d4d093a058fad9549ad96cd584100e603
Thota Yatin <thotayatin.vitv@gmail.com>
2026-09-06 18:30:46 +0530

    Build Level 1 map and intro sequence

--- Scenes/Level1.js
+++ Scenes/Level1.js
@@ -60,19 +60,38 @@ class Level1Scene extends Phaser.Scene {
 
         this.add.rectangle(1375, 734, 70, 1069, roadColor);
 
+        this.add.rectangle(1800, 725, 50, 600, roadColor);
+
         const road1 = this.add.rectangle(567,567,175,55,roadColor);
         road1.setRotation(Phaser.Math.DegToRad(30));
 
         const road2 = this.add.rectangle(815,1350,1200,60,roadColor);
         road2.setRotation(Phaser.Math.DegToRad(-10));
 
-        const road3 = this.add.rectangle(567,567,175,55,roadColor);
-        road3.setRotation(Phaser.Math.DegToRad(30));
+        const road3 = this.add.rectangle(1450,1400,410,60,roadColor);
+        road3.setRotation(Phaser.Math.DegToRad(70));
 
+
+        const road4 = this.add.rectangle(850,1550,410,60,roadColor);
+        road4.setRotation(Phaser.Math.DegToRad(80));
+
+        const road5 =this.add.rectangle(1570, 750, 450, 60, roadColor);
+        road5.setRotation(Phaser.Math.DegToRad(-10));
+
+        const road6 = this.add.rectangle(2000, 750, 400, 60, roadColor);
+        road6.setRotation(Phaser.Math.DegToRad(10));
+
+        const road7 = this.add.rectangle(2000, 1010, 400, 60, roadColor);
+        road7.setRotation(Phaser.Math.DegToRad(5));
         // Horizontal Connecting Roads
 
         this.add.rectangle(1000, 609, 750, 50, roadColor);
         this.add.rectangle(930, 225, 850, 50, roadColor);
+
+        this.add.rectangle(1600, 1000, 400, 50, roadColor);
+
+        this.add.rectangle(2000, 450, 400, 50, roadColor);
+        
 
 
         // =========================
